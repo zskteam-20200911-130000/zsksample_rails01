19d23bd64a3d56a28372b23d5e088d6a4d4c6fef
phyten <mikiyasu.obara@prominence-llc.co.jp>
2020-09-16 18:13:54 +0900

    viewとcontrollerの雛形を作成。中身は別のPRで行う。

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,23 @@
 class MicropostsController < ApplicationController
+  def index
+    # TODO: 追加予定
+  end
+  def show
+    # TODO: 追加予定
+  end
+  def new
+    # TODO: 追加予定
+  end
+  def create
+    # TODO: 追加予定
+  end
+  def edit
+    # TODO: 追加予定
+  end
+  def update
+    # TODO: 追加予定
+  end
+  def destroy
+    # TODO: 追加予定
+  end
 end
