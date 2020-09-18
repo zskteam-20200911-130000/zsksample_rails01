de1fe05245692f713c72da74a1c6d210dff3dfc8
wataru <skgch0702@gmail.com>
2020-09-18 16:47:58 +0900

    test: Userのモデルスペックへvalidationのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(name: user_name, email: user_email) }
+  let(:user_name) { 'test_user' }
+  let(:user_email) { 'test_user@test.com' }
+
+  describe 'test_user' do
+    it { expect(user).to be_valid }
+  end
+
+  describe 'validation' do
+    before { user.valid? }
+
+    context 'without a name' do
+      let(:user_name) { '' }
+
+      it { expect(user.errors[:name]).to include("can't be blank") }
+    end
+
+    context 'without a email' do
+      let(:user_email) { '' }
+
+      it { expect(user.errors[:email]).to include("can't be blank") }
+    end
+  end
 end
