d301ad265a8f53ed090fefa50103630394aa016b
wataru063 <61283756+wataru063@users.noreply.github.com>
2020-09-23 17:46:29 +0900

    Feature/associate user with microposts (#24)
    * bundle exec rails g model micropost
    
    * contentカラムを追加
    
    * usersを参照する外部キーの設定。referencesは使わず、外部キー制約はつけ
    ず。
    
    * bundle exec rails db:migrate
    
    * bundle exec rails g controller microposts
    
    * viewとcontrollerの雛形を作成。中身は別のPRで行う。
    
    * config/routes.rb にmicroposts関連追加
    
    * test: Userのモデルスペックへvalidationのテストを追加
    
    * refactor/rubocopに指摘された箇所の修正
    
    * feat: Associate user with microposts (#13)
    
    Co-authored-by: phyten <mikiyasu.obara@prominence-llc.co.jp>
    Co-authored-by: Soya Kasuga <edogawa.0255@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many   :microposts, class_name: 'Micropost', dependent: :destroy, inverse_of: :user
   validates :name, presence: true
   validates :email, presence: true
 end
