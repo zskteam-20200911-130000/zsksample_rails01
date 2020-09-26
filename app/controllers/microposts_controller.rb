73ad462258da06ba5e7054838ca3db5705a8c934
Soya Kasuga <62055598+SoyaKasuga@users.noreply.github.com>
2020-09-26 16:00:46 +0900

    feat:Micropostに基本的な処理を追加 (#30)
    * feat:Micropostに基本的な処理を追加
    
    * fix:最終行に空行を追加

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,29 +1,59 @@
 class MicropostsController < ApplicationController
+  before_action :set_micropost, only: %i[show edit update destroy]
+
   def index
-    # TODO: 追加予定
+    @microposts = Micropost.all
   end
 
-  def show
-    # TODO: 追加予定
-  end
+  def show; end
 
   def new
-    # TODO: 追加予定
+    @micropost = Micropost.new
   end
+
+  def edit; end
 
   def create
-    # TODO: 追加予定
-  end
+    @micropost = Micropost.new(micropost_params)
 
-  def edit
-    # TODO: 追加予定
+    respond_to do |format|
+      if @micropost.save
+        format.html { redirect_to @micropost, notice: 'Micropost was successfully created.' }
+        format.json { render :show, status: :created, location: @micropost }
+      else
+        format.html { render :new }
+        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
-    # TODO: 追加予定
+    respond_to do |format|
+      if @micropost.update(micropost_params)
+        format.html { redirect_to @micropost, notice: 'Micropost was successfully updated.' }
+        format.json { render :show, status: :ok, location: @micropost }
+      else
+        format.html { render :edit }
+        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
-    # TODO: 追加予定
+    @micropost.destroy
+    respond_to do |format|
+      format.html { redirect_to microposts_url, notice: 'Micropost was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_micropost
+    @micropost = Micropost.find(params[:id])
+  end
+
+  def micropost_params
+    params.require(:micropost).permit(:content, :user_id)
   end
 end
